993c92fbcc827f2d05f1c1542d996f9d6a7cc0e5
Idogwu Chinonso <idogwuchinonso1@gmail.com>
2024-06-11 20:07:14 +0100

    implemented tank factory

--- onchain_contracts/src/gasTankFactory.sol
+++ onchain_contracts/src/gasTankFactory.sol
@@ -1,8 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract GasTankFactory {
-    
+import "./gasTank.sol";
 
+contract GasTankFactory {
+    uint256 public totalGasTanks;
+    address public Relayer;
+    address public Admin;
+    address public CTSIToken;
+    address[] public allGasTanks;
+    mapping (address => address[]) public userToDappAddress;
+    mapping (address => gasTankDetails) public dappAddressToGasTankDetails;
+
+
+    struct gasTankDetails {
+        uint256 id;
+        address admin;
+        address tankAddress;
+        uint256 dateCreated;
+    }
+
+    event gasTankCreated(address creator, address tankAddress);
+
+    constructor(address relayer, address ctsi_Token) {
+        Relayer = relayer;
+        Admin = msg.sender;
+        CTSIToken = ctsi_Token;
+    }
    
+
+   function createGasTank(address contractAddress) external {
+        GasTank newGasTank = new GasTank(msg.sender, Relayer, address(this), CTSIToken, contractAddress);
+        gasTankDetails memory newTankDetails;
+
+        totalGasTanks++;
+        newTankDetails.id = totalGasTanks;
+        newTankDetails.admin = msg.sender;
+        newTankDetails.tankAddress = address(newGasTank);
+        newTankDetails.dateCreated = block.timestamp;
+
+        dappAddressToGasTankDetails[contractAddress] = newTankDetails;
+        userToDappAddress[msg.sender].push(contractAddress);
+        allGasTanks.push(address(newGasTank));
+
+        emit gasTankCreated(msg.sender, address(newGasTank));
+   }
+
+    function getGatTankAddress(address contractAddress) public view returns(address) {
+        return dappAddressToGasTankDetails[contractAddress].tankAddress;
+    }
+
 }
+
